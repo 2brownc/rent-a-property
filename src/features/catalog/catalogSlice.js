118fa856595c2bc1a32bcd3e330f15090233e1a1
brown <86018533+2brownc@users.noreply.github.com>
2022-09-08 14:54:54 +0530

    implement state

---
+++ src/features/catalog/catalogSlice.js
@@ -0,0 +1,98 @@
+import { createSlice, createAsyncThunk, current } from '@reduxjs/toolkit';
+import data from '../data/data';
+
+const initialState = {
+  catalog: data,
+  city: null,
+  price: 300000,
+  area: [500, 5000],
+  bedrooms: []
+};
+
+const catalogSlice = createSlice({
+  name: 'catalog',
+  initialState,
+  reducers: {
+    setCity: (state, action) => {
+      const city = action.payload;
+
+      return {
+        ...state,
+        city,
+      };
+    },
+    setPrice: (state, action) => {
+      const price = action.payload;
+
+      return {
+        ...state,
+        price,
+      };
+    },
+    setArea: (state, action) => {
+      const area = action.payload;
+
+      return {
+        ...state,
+        area,
+      };
+    },
+    setBedrooms: (state, action) => {
+      const bedrooms = action.payload;
+
+      return {
+        ...state,
+        bedrooms,
+      };
+    },
+    filterCatalog: (state, action) => {
+      const city = current(state.city);
+      const price = current(state.price);
+      const area = current(state.area);
+      const bedrooms = current(state.bedrooms);
+
+      const catalog = data;
+
+      //filter by city
+      if (city != null) {
+        catalog = catalog.filter((item) => {
+          item.city === city;
+        });
+      }
+
+      //filter by max price
+      catalog = catalog.filter((item) => {
+        item.price <= price;
+      });
+
+      //filter by area
+      catalog = catalog.filter((item) => {
+        return item.area >= area[0] && item.area <= area[1];
+      });
+
+      //filter by bedrooms
+      if (bedrooms.length > 0) {
+        catalog = catalog.filter((item) => {
+          return bedrooms.includes(item.bedrooms);
+        });
+      }
+
+      // save the filtered catalog to the state      
+      return {
+        ...state,
+        catalog,
+      };
+
+    },
+  },
+});
+
+export const { filterCatalog } = catalogSlice.action;
+
+export default catalogSlice.reducer;
+
+export const price = (state) => state.catalog.price;
+export const area = (state) => state.catalog.area;
+export const bedrooms = (state) => state.catalog.bedrooms;
+export const city = (state) => state.catalog.city;
+export const cityList = (state) => state.catalog.cityList;
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import catalogReducer from '../features/catalog/catalogSlice'
+
+export default configureStore({
+  reducer: {
+    catalog: catalogReducer,
+  },
+});
