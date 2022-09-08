5124b00e685ba25efda62124db9a2767e10e68e3
brown <86018533+2brownc@users.noreply.github.com>
2022-09-08 15:04:37 +0530

    sample catalog data

---
+++ src/data/data.js
@@ -0,0 +1,374 @@
+data = [
+  {
+    key: 1,
+    city: 'bangalore',
+    price: 24000,
+    bedrooms: 1,
+    area: 650,
+    image: 'images/b1.jpg',
+    bathrooms: 2,
+    address: 'Roadpalli, Prestone'
+  },
+  {
+    key: 2,
+    city: 'bangalore',
+    price: 60000,
+    bedrooms: 1,
+    bathrooms: 2,
+    area: 400,
+    address: 'Diamond Isle, 1-Royal Palms',
+    image: 'images/b2.png'
+  },
+  {
+    key: 3,
+    city: 'bangalore',
+    price: 70000,
+    bedrooms: 2,
+    bathrooms: 3,
+    area: 1800,
+    address: 'Apartment in Whitefield',
+    image: 'images/b3.png'
+  },
+  {
+    key: 4,
+    city: 'bangalore',
+    price: 35000,
+    bedrooms: 2,
+    bathrooms: 2,
+    area: 900,
+    address: 'Apartment in Bannerghatta Road',
+    image: 'images/b4.jpg'
+  },
+  {
+    key: 5,
+    city: 'bangalore',
+    price: 120000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 1100,
+    address: 'Neelkanth Valley, Ghatkopar (East)',
+    image: 'images/b5.jpg'
+  },
+  {
+    key: 6,
+    city: 'bangalore',
+    price: 55000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 1700,
+    address: 'Sobha City, Santorini',
+    image: 'images/b6.jpg'
+  },
+  {
+    key: 7,
+    city: 'bangalore',
+    price: 75000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 1800,
+    address: 'Mantri Webcity, Hennur',
+    image: 'images/b7.jpg'
+  },
+  {
+    key: 8,
+    city: 'bangalore',
+    price: 80000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 2000,
+    address: 'Sarjarpur Road, Ratan Colony',
+    image: 'images/b8.jpg'
+  },
+  {
+    key: 9,
+    city: 'bangalore',
+    price: 75000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 2500,
+    address: '1514 Street, Cambridge Layout',
+    image: 'images/b9.jpg'
+  },
+  {
+    key: 10,
+    city: 'bangalore',
+    price: 500000,
+    bedrooms: 4,
+    bathrooms: 5,
+    area: 5000,
+    address: 'Prestige Golfshire, Devanahalli Bangalore',
+    image: 'images/b10.jpg'
+  },
+  {
+    key: 11,
+    city: 'bangalore',
+    price: 500000,
+    bedrooms: 4,
+    bathrooms: 53,
+    area: 4500,
+    address: 'Epsilon Villa Plot, Old Airport Road Bangalore',
+    image: 'images/b11.jpg'
+  },
+  {
+    key: 12,
+    city: 'bangalore',
+    price: 400000,
+    bedrooms: 4,
+    bathrooms: 4,
+    area: 4000,
+    address: 'Embassy Boulevard, Yelahanka Bangalore',
+    image: 'images/b12.jpg'
+  },
+  {
+    key: 13,
+    city: 'hyderabad',
+    price: 95000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 3000,
+    address: 'Prestige Ivy League, Hitech City',
+    image: 'images/b13.jpg'
+  },
+  {
+    key: 14,
+    city: 'hyderabad',
+    price: 380000,
+    bedrooms: 4,
+    bathrooms: 5,
+    area: 5000,
+    address: 'Jubilee Hills',
+    image: 'images/b14.jpg'
+  },
+  {
+    key: 15,
+    city: 'hyderabad',
+    price: 7000,
+    bedrooms: 1,
+    bathrooms: 1,
+    area: 450,
+    address: 'Chikkadpally',
+    image: 'images/b15.jpg'
+  },
+  {
+    key: 16,
+    city: 'hyderabad',
+    price: 18000,
+    bedrooms: 1,
+    bathrooms: 1,
+    area: 600,
+    address: 'Gayatri Hills',
+    image: 'images/b16.jpg'
+  },
+  {
+    key: 17,
+    city: 'hyderabad',
+    price: 23000,
+    bedrooms: 2,
+    bathrooms: 3,
+    area: 1300,
+    address: 'Himayath Nagar',
+    image: 'images/b17.jpg'
+  },
+  {
+    key: 18,
+    city: 'hyderabad',
+    price: 36000,
+    bedrooms: 2,
+    bathrooms: 2,
+    area: 1300,
+    address: 'Kukatpally Housing Board Colony',
+    image: 'images/b18.jpg'
+  },
+  {
+    key: 19,
+    city: 'hyderabad',
+    price: 25000,
+    bedrooms: 2,
+    bathrooms: 2,
+    area: 700,
+    address: 'Hilen Street, Madhapur',
+    image: 'images/b19.jpg'
+  },
+  {
+    key: 20,
+    city: 'hyderabad',
+    price: 7500,
+    bedrooms: 3,
+    bathrooms: 1,
+    area: 1100,
+    address: 'Hitech City',
+    image: 'images/b20.jpg'
+  },
+  {
+    key: 21,
+    city: 'hyderabad',
+    price: 55000,
+    bedrooms: 3,
+    bathrooms: 1,
+    area: 3500,
+    address: 'Tilak Road, Manikonda',
+    image: 'images/b21.jpg'
+  },
+  {
+    key: 22,
+    city: 'hyderabad',
+    price: 55000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 2500,
+    address: 'Patel Road, Kokapet',
+    image: 'images/b22.jpg'
+  },
+  {
+    key: 23,
+    city: 'hyderabad',
+    price: 70000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 2000,
+    address: 'Hyder Marg, Hitech City',
+    image: 'images/b23.jpg'
+  },
+  {
+    key: 24,
+    city: 'mumbai',
+    price: 90000,
+    bedrooms: 4,
+    bathrooms: 6,
+    area: 2100,
+    address: 'Runwal Greens, Mulund West',
+    image: 'images/m1.jpg'
+  },
+  {
+    key: 25,
+    city: 'mumbai',
+    price: 330000,
+    bedrooms: 4,
+    bathrooms: 43,
+    area: 1600,
+    address: 'Trump Tower, Worli',
+    image: 'images/m25.jpg'
+  },
+  {
+    key: 26,
+    city: 'mumbai',
+    price: 140000,
+    bedrooms: 4,
+    bathrooms: 4,
+    area: 1400,
+    address: 'Insignia, Bandra East',
+    image: 'images/m26.jpg'
+  },
+  {
+    key: 27,
+    city: 'mumbai',
+    price: 110000,
+    bedrooms: 3,
+    bathrooms: 2,
+    area: 1100,
+    address: 'Lodha New Cuffe Parade, Wadala East',
+    image: 'images/m27.jpg'
+  },
+  {
+    key: 28,
+    city: 'mumbai',
+    price: 55000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 950,
+    address: 'Sheth Montana, Mulund West',
+    image: 'images/m28.jpg'
+  },
+  {
+    key: 29,
+    city: 'mumbai',
+    price: 200000,
+    bedrooms: 3,
+    bathrooms: 4,
+    area: 1700,
+    address: 'Bombay Dyeing Two ICC, Dadar East',
+    image: 'images/m29.jpg'
+  },
+  {
+    key: 30,
+    city: 'mumbai',
+    price: 300000,
+    bedrooms: 3,
+    bathrooms: 3,
+    area: 2200,
+    address: 'World One Tower',
+    image: 'images/m30.jpg'
+  },
+  {
+    key: 31,
+    city: 'mumbai',
+    price: 85000,
+    bedrooms: 2,
+    bathrooms: 2,
+    area: 1000,
+    address: 'Lodha New Cuffe Parade, Bhakti Park',
+    image: 'images/m31.jpg'
+  },
+  {
+    key: 32,
+    city: 'mumbai',
+    price: 85000,
+    bedrooms: 2,
+    bathrooms: 2,
+    area: 1000,
+    address: 'Celestia Spaces, Sewri',
+    image: 'images/m32.jpg'
+  },
+  {
+    key: 33,
+    city: 'mumbai',
+    price: 140000,
+    bedrooms: 2,
+    bathrooms: 2,
+    area: 100,
+    address: 'Lodha, Park Side',
+    image: 'images/m33.jpg'
+  },
+  {
+    key: 34,
+    city: 'mumbai',
+    price: 63500,
+    bedrooms: 1,
+    bathrooms: 13,
+    area: 500,
+    address: 'Lodha New Cuffe Parade, Bhakti Park',
+    image: 'images/m34.jpg'
+  },
+  {
+    key: 35,
+    city: 'mumbai',
+    price: 35000,
+    bedrooms: 1,
+    bathrooms: 2,
+    area: 450,
+    address: 'Sindhi Society, Chembur',
+    image: 'images/m35.jpg'
+  },
+  {
+    key: 36,
+    city: 'mumbai',
+    price: 25000,
+    bedrooms: 1,
+    bathrooms: 2,
+    area: 550,
+    address: 'Jaswanti Woods, Mulund West',
+    image: 'images/m36.jpg'
+  },
+  {
+    key: 37,
+    city: 'mumbai',
+    price: 33000,
+    bedrooms: 1,
+    bathrooms: 2,
+    area: 600,
+    address: 'Comet Apartment, Bhakti Park',
+    image: 'images/m37.jpg'
+  },
+];
+
+export default data;
