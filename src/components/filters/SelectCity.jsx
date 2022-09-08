f338d095f467f03b1f2e6a7ea1b9052007b21d3f
brown <86018533+2brownc@users.noreply.github.com>
2022-09-08 14:55:28 +0530

    get/set values from store

--- src/components/filters/SelectCity.jsx
+++ src/components/filters/SelectCity.jsx
@@ -3,16 +3,19 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 
-const cityList = [
-  { label: "Bangalore" },
-  { label: "Pune" },
-  { label: "Hyderabad" },
-  { label: "Mumbai" },
-  { label: "Chennai" },
-];
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  city,
+  setCity,
+  cityList,
+} from '../../features/catalog/catalogSlice';
 
 export default function SelectCity(/*cityList, city, setCity*/) {
-  const [value, setValue] = useState(cityList[0]);
+  const cityValue = useSelector(city);
+  const cityListValue = useSelector(cityList);
+  const dispatch = useDispatch();
+
+  const [value, setValue] = useState(cityListValue[0]);
   const [inputValue, setInputValue] = useState('');
 
   return (
@@ -21,13 +24,14 @@ export default function SelectCity(/*cityList, city, setCity*/) {
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
+          dispatch(setCity(newValue));
         }}
         inputValue={inputValue}
         onInputChange={(event, newInputValue) => {
           setInputValue(newInputValue);
         }}
         id="controllable-states-demo"
-        options={cityList}
+        options={cityListValue}
         sx={{ width: "100%" }}
         renderInput={(params) => <TextField {...params} label="Select City" />}
       />
