9cf756d7e6ac2c23c38c81cdc34c90c37154736b
brown <86018533+2brownc@users.noreply.github.com>
2022-09-08 19:20:18 +0530

    add steps to slider

--- src/components/filters/SelectAreaRange.jsx
+++ src/components/filters/SelectAreaRange.jsx
@@ -79,6 +79,7 @@ export default function SelectPrice() {
             disableSwap
             min={minArea}
             max={maxArea}
+            step={step}
           />
         </Grid>
       </Grid>
